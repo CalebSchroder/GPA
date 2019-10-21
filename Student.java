0abc7f3c138741c6135eb6880309ecb177ddd4e9
Caleb Schroder <doomskier@gmail.com>
2019-10-21 12:52:05 -0600

    start

---
+++ Student.java
@@ -0,0 +1,52 @@
+public class Student {
+
+    // data - properties
+    private String name;
+    private int gradeLevel;
+    
+    private String mathScore;
+    private String englishScore;
+    private String csScore;
+
+    // constructor
+    public Student(String name, int gradeLevel) {
+        this.name = name;
+        this.gradeLevel = gradeLevel;
+
+        this.mathScore = "F";
+        this.englishScore = "F";
+        this.csScore = "F";
+    }
+
+    // functions - abilities
+
+    // getters and setters
+    
+    public String getName() {
+        return this.name;
+    }
+
+    public int getGradeLevel() {
+        return = this.gradeLevel;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public void setGradeLevel(int gradeLevel) {
+        if (gradeLevel >= 9 && gradeLevel <= 12) {
+            this.gradeLevel = gradeLevel;
+        }
+    }
+
+    // calculate gpa
+
+    public double calculateGPA() {
+        System.out.println("Calculating GPA...");
+
+        return 0.0; 
+    }
+
+
+}
